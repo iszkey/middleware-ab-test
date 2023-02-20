d151cbc7fac205a76a1e7b04b4d305e60515ab02
iszkey <kisozaki1026@gmail.com>
2023-02-20 22:15:32 +0900

    middlewareの実装

--- pages/renewal.tsx
+++ pages/renewal.tsx
@@ -4,8 +4,8 @@ import { CubeTransparentIcon } from '@heroicons/react/solid';
 const Home: NextPage = () => {
     return (
         <div className="font-memo flex min-h-screen flex-col items-center justify-center text-gray-800">
-            <p className="text-ml my-3">Concept A</p>
-            <CubeTransparentIcon className="h-12 w-12 text-blue-500" />
+            <p className="text-ml my-3">Concept B</p>
+            <CubeTransparentIcon className="h-12 w-12 text-pink-500" />
         </div>
     );
 };
